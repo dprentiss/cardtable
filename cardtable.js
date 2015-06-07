a291556f253aea4c3d0efb609f11e0410eb5f61a
dprentiss <dprentiss@gmail.com>
2015-06-07 08:32:14 -0400

    refactor card orientation function

--- cardtable.js
+++ cardtable.js
@@ -1,68 +1,64 @@
 $(document).ready(function() {
-    $('#periodCardRow').disableSelection().sortable({revert: 100});
+  $('#periodCardRow').disableSelection().sortable({revert: 100});
 });
 
 var newPeriodHandler = function() {
-    this.numCards = (this.numCards || 0) + 1;
-    newPeriod = $("<div>", {class: "periodColumn"});
-    newPeriod.disableSelection().sortable({revert: 100});
-    newCard = $("<div>", {class: "card"});
-    newCard.append("<h1>" + numCards + "</h1>");
-    newCard.append("<button>Add Event</button>");
-    newPeriod.append(newCard);
-    $("#periodCardRow").append(newPeriod);
+  this.numCards = (this.numCards || 0) + 1;
+  newPeriod = $("<div>", {class: "periodColumn"});
+  newPeriod.disableSelection().sortable({revert: 100});
+  newCard = $("<div>", {class: "card"});
+  newCard.append("<h1>" + numCards + "</h1>");
+  newCard.append("<button>Add Event</button>");
+  newPeriod.append(newCard);
+  $("#periodCardRow").append(newPeriod);
 }
 
 var CardGame = function() {
-    this.createdDate = new Date();
+  this.createdDate = new Date();
 };
 
 var Table = function() {
-    this.createdDate = new Date();
-    this.stacks = [];
+  this.createdDate = new Date();
+  this.stacks = [];
 }
 
 Table.prototype.addStack = function(stack, location) {
-    this.stacks.push({stack: stack, location: location});
+  this.stacks.push({stack: stack, location: location});
 }
 
 var Stack = function() {
-    this.createdDate = new Date();
-    this.cards = [];
+  this.createdDate = new Date();
+  this.cards = [];
 };
 
 Stack.prototype.addCard = function(card, orientation) {
-    this.cards.push({card: card, orientation: orientation});
+  this.cards.push({card: card, orientation: orientation});
 }
 
 var Card = function() {
-    this.createdDate = new Date();
-    this.isLocked = false;
-    this.front = 'Front';
-    this.back = 'Back';
+  this.createdDate = new Date();
+  this.isLocked = false;
+  this.front = 'Front';
+  this.back = 'Back';
 };
 
 Card.prototype.toggleOrientation = function() {
-    if (this.orientation == 'portrait') {
-        this.orientation = 'landscape';
-    } else if (this.orientation == 'landscape') {
-        this.orientation = 'portrait';
-    }    
+  (this.orientation == 'portrait' ? this.orientation = 'landscape' : this.orientation = 'portrait');
 };
 
 Card.prototype.toggleLock = function() {
-    if (this.isLocked) {this.isLocked = false;}
-    else {this.isLocked = true;}
+  if (this.isLocked) {this.isLocked = false;}
+  else {this.isLocked = true;}
 };
 
 var test = function() {
-    cardGame = new CardGame();
-    table = new Table();
-    stack = new Stack();
-    for (i = 0; i < 3; i++) {
-        stack.addCard(new Card(), 'portrait');
-    }
-    table.addStack(stack, 'center')
+  cardGame = new CardGame();
+  table = new Table();
+  stack = new Stack();
+  for (i = 0; i < 3; i++) {
+    stack.addCard(new Card(), 'portrait');
+  }
+  table.addStack(stack, 'center')
 }
 
 //test()
